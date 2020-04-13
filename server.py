a55c74d3fa96786d69de8e4d3c32d52e18d8ef7a
Nitish Dwivedi <nitish.dwi@gmail.com>
2020-04-13 22:26:38 +0530

    removed redundent code from server.py

--- server.py
+++ server.py
@@ -137,27 +137,6 @@ def logout():
     params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
-@celery.task(bind=True)
-def create_user(self, email):
-    new_user = User(email=email)
-    db.session.add(new_user)
-    db.session.commit()
-    self.update_state(state='COMPLETED')
-    logging.info("Created")
-    return 1
-
-@celery.task(bind=True)
-def create_vid(self, user_id, vid_name):
-    vid = Video(user_id, vid_name)
-    db.session.add(vid)
-    db.session.commit()
-
-@celery.task(bind=True)
-def add_vid_path(self, video_id):
-    vid = Video.query.filter_by(id=video_id).first()
-    vid.video_path = str(vid.user_id) + "/" + str(video_id)
-    db.session.add(vid)
-    db.session.commit()
 
 def generate_emotion_video(ray_list, vid_path, size):
     cap = cv2.VideoCapture(vid_path)
