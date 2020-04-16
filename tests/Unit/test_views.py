faefa004b4152d7bfb928927432cbf02c1c4e46d
Manan Goel <manan.goel@research.iiit.ac.in>
2020-04-16 11:59:43 +0530

    Added basic testing framework

---
+++ tests/Unit/test_views.py
@@ -0,0 +1,6 @@
+from flask import url_for
+
+class TestPage(object):
+    def test_home_page(self, client):
+        response = client.get(url_for('home'))
+        assert response.status_code == 200
