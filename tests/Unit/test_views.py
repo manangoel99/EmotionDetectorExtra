d014e14f692ba94f7709b392f38053ce93ae9dc0
Manan Goel <manan.goel@research.iiit.ac.in>
2020-04-19 21:18:07 +0530

    Added test for video upload

--- tests/Unit/test_views.py
+++ tests/Unit/test_views.py
@@ -3,6 +3,7 @@ from models import User, Video
 import constants
 import shutil
 import os
+import io
 
 class TestPage(object):
     def test_home_page(self, client):
@@ -66,3 +67,39 @@ class TestPage(object):
         user = User.query.filter_by(email="test_user@gmail.com").first()
         db.session.delete(user)
         db.session.commit()
+
+    def test_video_upload(self, client, db):
+        new_user = User(email="test_user@gmail.com")
+        db.session.add(new_user)
+        db.session.commit()
+        with client.session_transaction() as sess:
+            sess[constants.JWT_PAYLOAD] = {
+                "email" : new_user.email
+            }
+            sess[constants.PROFILE_KEY] = {
+                "user_id" : "test_user",
+                "name" : "Test User"
+            }
+        
+        data = {}
+        data['file'] = (io.BytesIO(b"abcdef"), "./testvdo.mp4")
+
+        response = client.post(
+            url_for('upload_file'),
+            data=data,
+            follow_redirects=True,
+            content_type='multipart/form-data'
+        )
+
+        vid = Video.query.all()[-1]
+
+        user = User.query.filter_by(email="test_user@gmail.com").first()
+
+        assert vid.user_id == user.id
+        assert vid.video_path == os.path.join(str(user.id), str(vid.id))
+
+        shutil.rmtree(os.path.join("/home/app/public", str(user.id)))
+        db.session.delete(user)
+        db.session.commit()
+
+
