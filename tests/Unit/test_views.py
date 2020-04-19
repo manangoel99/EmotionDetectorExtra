ec570aa2cf00ffd8f8b958e8d1cdbbe8ba7a55df
Manan Goel <manan.goel@research.iiit.ac.in>
2020-04-19 22:00:19 +0530

    Tested video processing

--- tests/Unit/test_views.py
+++ tests/Unit/test_views.py
@@ -4,6 +4,8 @@ import constants
 import shutil
 import os
 import io
+import time
+from werkzeug.datastructures import FileStorage
 
 class TestPage(object):
     def test_home_page(self, client):
@@ -19,6 +21,7 @@ class TestPage(object):
         assert db.session.query(User).one()
         
         user = User.query.filter_by(email="test_user@gmail.com").first()
+        print(os.listdir("/home/app/public"))
         db.session.delete(user)
         db.session.commit()
         user = User.query.filter_by(email="test_user@gmail.com").first()
@@ -49,12 +52,9 @@ class TestPage(object):
         assert len(videos) == 0
 
     def test_dashboard(self, client, db):
-        new_user = User(email="test_user@gmail.com")
-        db.session.add(new_user)
-        db.session.commit()
         with client.session_transaction() as sess:
             sess[constants.JWT_PAYLOAD] = {
-                "email" : new_user.email
+                "email" : "test_user@gmail.com"
             }
             sess[constants.PROFILE_KEY] = {
                 "user_id" : "test_user",
@@ -65,21 +65,29 @@ class TestPage(object):
         assert res.status_code == 200
         assert "Welcome" in str(res.data)
         user = User.query.filter_by(email="test_user@gmail.com").first()
+        assert user != None
+        shutil.rmtree(os.path.join("/home/app/public", str(user.id)))
+
         db.session.delete(user)
         db.session.commit()
+        user = User.query.filter_by(email="test_user@gmail.com").first()
+        assert user == None
 
     def test_video_upload(self, client, db):
-        new_user = User(email="test_user@gmail.com")
-        db.session.add(new_user)
-        db.session.commit()
+        # new_user = User(email="test_user@gmail.com")
+        # db.session.add(new_user)
+        # db.session.commit()
         with client.session_transaction() as sess:
             sess[constants.JWT_PAYLOAD] = {
-                "email" : new_user.email
+                "email" : "test_user@gmail.com"
             }
             sess[constants.PROFILE_KEY] = {
                 "user_id" : "test_user",
                 "name" : "Test User"
             }
+
+        res = client.get(url_for('dashboard'))
+        assert res.status_code == 200
         
         data = {}
         data['file'] = (io.BytesIO(b"abcdef"), "./testvdo.mp4")
@@ -96,10 +104,58 @@ class TestPage(object):
         user = User.query.filter_by(email="test_user@gmail.com").first()
 
         assert vid.user_id == user.id
+        assert os.path.exists(os.path.join("/home/app/public", str(user.id), str(vid.id)))
         assert vid.video_path == os.path.join(str(user.id), str(vid.id))
-
-        shutil.rmtree(os.path.join("/home/app/public", str(user.id)))
+        try:
+            shutil.rmtree(os.path.join("/home/app/public", str(user.id)))
+        except:
+            print("Folder Not Created")
         db.session.delete(user)
         db.session.commit()
 
 
+    def test_video_processing(self, client, db):
+        with client.session_transaction() as sess:
+            sess[constants.JWT_PAYLOAD] = {
+                "email" : "test_user@gmail.com"
+            }
+            sess[constants.PROFILE_KEY] = {
+                "user_id" : "test_user",
+                "name" : "Test User"
+            }
+
+        res = client.get(url_for('dashboard'))
+        assert res.status_code == 200
+        my_file = FileStorage(
+            stream=open(os.path.join("/home/app/tests", "testvdo.mp4")),
+            filename="my_video.mp4",
+            content_type="video/mpeg"
+        )
+        data = {}
+        data['file'] = io.open("/home/app/tests/testvdo.mp4", "rb", buffering=0)
+        print(my_file)
+        response = client.post(
+            url_for('upload_file'),
+            data=data,
+            follow_redirects=True,
+            content_type='multipart/form-data'
+        )
+
+        vid = Video.query.all()[-1]
+
+        user = User.query.filter_by(email="test_user@gmail.com").first()
+
+        assert vid.user_id == user.id
+        assert vid.video_path == os.path.join(str(user.id), str(vid.id))
+
+        time.sleep(45)
+        
+
+        assert os.path.exists(os.path.join("/home/app/public", str(user.id), str(vid.id) + "_emotion.webm"))
+
+        try:
+            shutil.rmtree(os.path.join("/home/app/public", str(user.id)))
+        except:
+            pass
+        db.session.delete(user)
+        db.session.commit()
