45abb39ffa66e27dc12934e87bd1b9263b87ef29
dvijaywargiya <dvijaywargiya@gmail.com>
2020-04-26 23:35:44 +0530

    Added an Error Handler for all HTTP errors

--- server.py
+++ server.py
@@ -510,3 +510,15 @@ def getVidStatus():
     except:
         user = None
     return jsonify(vids_json)
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
